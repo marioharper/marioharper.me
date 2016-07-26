6e0b503040e5199b93b16bfa86f5075c9436ed4f
Mario Harper <Mario Harper>
2016-07-26 16:41:54 -0500

    Update 2016-07-26T21:41:52.982Z

--- tasks/clean.js
+++
@@ -1,8 +0,0 @@
-var gulp     	= require('gulp'),
-		clean 		= require('gulp-clean'),
-  	paths     = require('../paths');
-
-// Clean build folder
-gulp.task('clean', function(){
-	return gulp.src(paths.output, {read: false}).pipe(clean());
-});
