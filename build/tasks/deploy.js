eca1cc8c8d3a954cd52f538a42383bed18e6d65e
Mario Harper <harpmari12@gmail.com>
2016-12-12 20:55:44 -0600

    remove deploy task

--- build/tasks/deploy.js
+++
@@ -1,8 +0,0 @@
-var gulp 				= require('gulp'),
-  paths     = require('../paths');
-
-/*copy over CNAME file and deploy build folder*/
-gulp.task('deploy', function () {
-  gulp.src('src/CNAME')
-    .pipe(gulp.dest(paths.output));
-});
